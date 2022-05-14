595e4cafef0a07d33c5b1e4b4f23e710625d60b4
Aditya verma <vermapradeeep5@gmail.com>
2022-05-14 22:05:00 +0530

    AvtoBus

--- AvtoBus.cpp
+++ AvtoBus.cpp
@@ -22,6 +22,56 @@ int main()
             if(n%6==0&&n%4==0){
                 cout<<n/6<<" "<<n/4<<endl;
             }
+            else if(n%6==0){
+                cout<<n/6<<" "<<n/6<<endl;
+            }
+            else{
+                cout<<n/4<<" "<<n/4<<endl;
+            }
+        }
+    }
+    return 0;
+}
+
+//== == == == == == == == == == == == == == == == == == == == == == == == == == == == ==
+
+#include <bits/stdc++.h>
+    using namespace std;
+
+#define lli long long int
+
+lli maxi(lli n)
+{
+    return n / 4;
+}
+
+lli mini(lli n)
+{
+    if (n % 6 != 0)
+    {
+        return n / 6 + 1;
+    }
+    else
+    {
+        return n / 6;
+    }
+}
+
+int main()
+{
+    lli t;
+    cin >> t;
+    while (t--)
+    {
+        lli n;
+        cin >> n;
+        if (n % 2 != 0)
+        {
+            cout << -1 << endl;
+        }
+        else
+        {
+            cout << mini(n) << " " << maxi(n) << endl;
         }
     }
     return 0;
